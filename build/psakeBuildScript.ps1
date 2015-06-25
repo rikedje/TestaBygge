847c7d46f646a65d36798448f4c195db19267dc0
rikedje <rikard.edje@gmail.com>
2015-06-25 19:57:13 +0200

    adding bat

--- build/psakeBuildScript.ps1
+++ build/psakeBuildScript.ps1
@@ -14,25 +14,30 @@ Task default -Depends CreateNugetPackage
 Task Deploy -Depends PushNugetPackageToOctopusDeploy
 
 Task Clean {
-  Remove-Item $build_script_dir\*.nupkg -force
+    Remove-Item $build_script_dir\*.nupkg -force
 }
 
 Task CreateNugetPackage -Depends Clean {
   Update-NuspecVersionNumber
   $nuget_exe = (Get-ChildItem "$build_script_dir\tools\NuGet.exe" | Select-Object -First 1)
-  Exec { &$nuget_exe "pack" $nuget_spec -NoPackageAnalysis }
+  $cmd = "$nuget_exe pack $nuget_spec -NoPackageAnalysis"
+  $sb = [ScriptBlock]::Create($cmd)
+  Exec $sb
 }
 
 Task PushNugetPackageToOctopusDeploy -Depends CreateNugetPackage {
   $nuget_exe = (Get-ChildItem "$build_script_dir\tools\NuGet.exe" | Select-Object -First 1)
   $octopus_package = (Get-ChildItem "$build_script_dir\*.nupkg" | Select-Object -Last 1)
   
-  Write-Host "Publish $octopus_package to $octo_serverUrl/nuget/packages"
-  Exec { &$nuget_exe push $octopus_package -ApiKey $octo_apiKey -Source $octo_serverUrl/nuget/packages }
+  "Publishing $octopus_package to $octo_serverUrl/nuget/packages"
+  $cmd = "$nuget_exe push $octopus_package -ApiKey $octo_apiKey -Source $octo_serverUrl/nuget/packages"
+  $sb = [ScriptBlock]::Create($cmd)
+  Exec $sb
 }
 
 function Update-NuspecVersionNumber {
   # update nuget spec version number
+  "Updating versionnumber to $designated_versionNumber"
   [xml]$spec = Get-Content $nuget_spec -Encoding UTF8
   $spec.package.metadata.version = $designated_versionNumber
   Set-ItemProperty $nuget_spec IsReadOnly $false
